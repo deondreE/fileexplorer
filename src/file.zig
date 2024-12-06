9ab200f455b37b82b41ba394d39ac42ab76f8f0f
deondreE <deondreenglish45@gmail.com>
2024-12-06 01:48:12 +0000

    added the btree to the backend architecture

--- src/file.zig
+++ src/file.zig
@@ -2,6 +2,104 @@ const std = @import("std");
 const fs = std.fs;
 const heap = std.heap;
 
+pub const Node = struct {
+    keys: []*const u8,
+    t: i32,
+    C: []Node,
+    leaf: bool,
+    kNum: i32, // current number of keys.
+
+    pub fn search(self: Node, key: *const u8) *Node {
+        var i: i32 = 0;
+
+        if (self.leaf)
+            return null;
+
+        while (i < self.kNum) : (i += 1) {
+            if (self.keys[i] == key)
+                return &self;
+        }
+
+        return self.C[i].search(key);
+    }
+
+    pub fn splitChild(self: Node, value: *const []u8, rootNode: *Node) void {
+        // TODO: do this.
+    }
+
+    pub fn insertNonFull(self: Node, key: *const []u8) void {
+        var i: i32 = self.kNum - 1;
+
+        if (leaf) {
+            // TODO: find location of new keys.
+        }
+    }
+
+    pub fn traverse(self: Node) void {
+        var i: i32 = 0;
+        while (i < self.kNum) : (i += 1) {
+            if (!self.leaf)
+                self.C[i].traverse();
+            std.debug.print("{s}", .{self.keys[i]});
+        }
+
+        if (!self.leaf)
+            self.C[i].traverse();
+    }
+};
+
+pub const Tree = struct {
+    root: *Node,
+    leaves: []Node,
+    t: u32,
+
+    pub fn init() Tree {}
+
+    pub fn insert(self: Tree, key: *const []u8) Tree {
+        if (self.root == null) {
+            self.root = Node{
+                .keys = key,
+                .kNum = 1,
+            };
+        } else {
+            if (self.root.kNum == 2) {
+                var node = Node{
+                    .kNum = self.t,
+                    .leaf = false,
+                };
+
+                // Make old root an move 1 key to the new root.
+                node.C[0] = self.root;
+                node.splitChild("", self.root);
+                var i: i32 = 0;
+                if (node.keys[0] == key)
+                    i += 1;
+
+                node.C[i].insertNonFull(key);
+
+                self.root = node;
+            } else {
+                self.root.insertNonFull(key);
+            }
+        }
+    }
+
+    pub fn delete(node: Node) Tree {}
+
+    pub fn traverse(self: Tree) void {
+        if (self.root != null)
+            self.root.traverse();
+    }
+
+    pub fn search(self: Tree, key: []*const u8) *Node {
+        if (self.root == null) {
+            return null;
+        }
+
+        return self.root.search(key);
+    }
+};
+
 pub const FS = struct {
     start: []const u8,
 
