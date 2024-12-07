32708ec41af7901260a113e6f1be7e5591941936
deondreE <deondreenglish45@gmail.com>
2024-12-07 05:35:08 +0000

    added stack from https://www.huy.rocks/toylisp/01-08-2022-zig-case-study-implementing-a-generic-stack; removed unnesissary files; starting work on gui process.

--- src/gui.zig
+++ src/gui.zig
@@ -1,8 +1,44 @@
 const std = @import("std");
 const rl = @import("raylib");
 
-pub const Layout = struct {
-    pub fn init() Layout {}
-
-    pub fn pushWidget() void {}
+pub var Size = struct {
+    width: i32,
+    height: i32,
 };
+
+pub var Position = struct {
+    x: i32,
+    y: i32,
+};
+
+pub const Widget = struct {
+    size: Size,
+    position: Position,
+
+    pub fn init(size: Size, position: Position) Layout {
+        return Widget{
+            .size = size,
+            .position = position,
+        };
+    }
+};
+
+pub const Layout = struct {
+    startPos: Position,
+
+    pub fn init(startPos: Position) Layout {
+        return Layout{
+            .startPos = startPos,
+        };
+    }
+
+    // calculate each position inside the layout inside here.
+    pub fn calculate() void {}
+
+    // push a given widget to the stack to be rendered when it is needed. If not do not render.
+    pub fn pushWidget(self: Layout, newWidget: Widget) !void {}
+
+    pub fn render() void {}
+};
+
+pub const Label = struct {};
